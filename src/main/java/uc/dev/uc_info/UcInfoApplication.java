9cfbb7df6dca117b7279f01217866e8efab5839d
Hanguyun <gksrndbs1907@gmail.com>
2026-05-11 11:47:03 +0900

    add - login page css/html - dashboard css/html - 각각의 Controller
    del
    - Test file

--- src/main/java/uc/dev/uc_info/UcInfoApplication.java
+++ src/main/java/uc/dev/uc_info/UcInfoApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class UcInfoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(UcInfoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(UcInfoApplication.class, args);
+    }
 
 }
---
+++ src/main/java/uc/dev/uc_info/controller/LoginController.java
@@ -0,0 +1,18 @@
+package uc.dev.uc_info.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/")
+    public String loginPage() {
+        return "login";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+}
